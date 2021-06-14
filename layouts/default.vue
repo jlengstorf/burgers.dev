fa3116fd7ad503185df94262912d98550e5b9bc8
Jason Lengstorf <jason@lengstorf.com>
2021-06-13 19:14:09 -0700

    feat: favicon, social images

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,7 @@
 <script>
 export default {
   head: {
+    title: 'Burger Showdown: Splash vs. Smash',
     link: [
       {
         rel: 'preconnect',
@@ -18,7 +19,71 @@ export default {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Bangers&display=swap'
       },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#ffb905',
+      },
     ],
+    meta: [
+      {
+        name: 'msapplication-TileColor',
+        content: '#ffb905',
+      },
+      {
+        name: 'theme-color',
+        content: '#ffb905',
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        name: 'twitter:creator',
+        content: '@jlengstorf',
+      },
+      {
+        name: 'twitter:creator',
+        content: '@sdras',
+      },
+      {
+        name: 'og:title',
+        content: 'Burger Showdown: Splash vs. Smash',
+      },
+      {
+        name: 'og:description',
+        content: 'Sarah Drasner and Jason Lengstorf face off in an epic excuse to feed their friends lots of cheeseburgers while talking shit to each other.',
+      },
+      {
+        name: 'og:image',
+        content: 'https://www.burgers.dev/images/social-card.jpg',
+      },
+      {
+        name: 'og:image:alt',
+        content: 'A burger with crossed spatulas below it and the text, "Who will be the burger champ, and who will the burger chump?"'
+      },
+    ]
   },
 };
 </script>
