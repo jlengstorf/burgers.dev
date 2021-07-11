aa0dadc0b2ce26640cf8aea9ef23f675a6c29797
Sarah Drasner <sarah.drasner@gmail.com>
2021-07-11 13:37:51 -0600

    update the description

--- components/SousVideBurger.vue
+++ components/SousVideBurger.vue
@@ -2,10 +2,36 @@
   <div>
     <SousVideBurgerDiagram />
     <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi quia
-      itaque blanditiis numquam provident omnis reprehenderit, doloribus nulla
-      quod eum cupiditate vel rerum nam nesciunt veniam sequi eos expedita
-      aliquam.
+      You may be skeptical if you’ve never tried, heard of, or worked with a
+      sous vide. That’s ok! There’s a reason this method is most well-known for
+      cooking expensive meats to perfection (think of a succulent filet mignon,
+      medium rare edge to edge, then seared for the perfect crust).
+    </p>
+    <p>
+      Grilling hamburgers, especially thick, juicy ones, are trickier than
+      grilling a steak. Burgers can fall apart, with a charred outside and stay
+      raw in the middle. This method slowly and precisely cooks so that it
+      reaches the exact optimum temperature and comes out perfectly every time.
+    </p>
+    <p>
+      The sous vide hamburger recipe produces a mouthwatering thick, juicy
+      burger, and you’re still chargrilling the outside for that deep, rich
+      crust.
+    </p>
+    <p>
+      As Jason mentioned, great hamburgers start with the right ground beef.
+      It’s best to use an 80/20 mix, meaning 20% fat (Jason is sometimes right,
+      even a blind squirrel finds a few acorns). Fortunately, we live in Denver,
+      where the quality of meat is just incredible, even from Food n Stuff. You
+      can go to a butcher and get fancier meat as well. Thankfully, this is cow
+      country, so it's hard to get it wrong.
+    </p>
+    <p>
+      In terms of cheese and other ingredients, I have my faves, but you
+      probably like your burger with particular condiments, and that’s just
+      fine. All I’ll say is that the bread and vegetables have to be fresh. A
+      freshly baked bun will be better than the fanciest bun that’s sat around
+      for a while, any day. Fresh is key.
     </p>
   </div>
 </template>
