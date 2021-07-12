21cc60124db84633862094c4843a4a3836a3e55b
Jason Lengstorf <jason@lengstorf.com>
2021-07-11 18:38:59 -0700

    fix: smashburger on small screens

--- components/SmashBurgerDiagram.vue
+++ components/SmashBurgerDiagram.vue
@@ -338,16 +338,7 @@
   position: relative;
   height: 0;
   padding-bottom: 52.35%;
-  width: 500px;
-}
-
-@media (min-width: 600px) {
-  .responsive-container {
-    margin-left: calc((var(--width) - (500px + 4rem)) / 2 * -1);
-    margin-right: calc((var(--width) - (500px + 4rem)) / 2 * -1);
-    width: var(--width);
-    height: calc(var(--width) * 0.5235);
-  }
+  width: 100%;
 }
 
 .details-thingy {
